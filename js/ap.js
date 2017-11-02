dd4d461df72fe870d96125ffd143d3d12a4088d3
Intesar Alam <alam.kir@gmail.com>
2017-11-03 00:05:55 +0530

    021120170

---
+++ js/ap.js
@@ -0,0 +1,53 @@
+﻿var app = angular.module('MyApp', ['ngRoute']);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+    .when('/', {
+        templateUrl: 'home.html'
+    })
+    .when('/dashboard', {
+       
+        resolve: {
+            "check": function ($location, $rootScope) {
+                if (!$rootScope.isLoggedIn) {
+                    $location.path('/');
+                }
+            }
+        },
+        templateUrl: 'dashboard.html'
+    })
+    .otherwise({
+        redirectTo:'/'
+    });
+});
+
+app.controller('MainCtrl', function ($scope, $location, $rootScope) {
+    $scope.name = 'World';
+    $rootScope.loggedInUser = '';
+    $scope.message = '';
+    $scope.myusers = [{
+        username: 'user1',
+        password: '12323'
+    }, {
+        username: 'user2',
+        password: '12323'
+    }];
+    $scope.submit = function () {
+        if ($scope.username && $scope.password) {
+            var user = $scope.username;
+            var pass = $scope.password;
+            $scope.message = "welcome " + user;
+            $rootScope.isLoggedIn = true;
+            $rootScope.loggedInUser = user;
+            //$scope.myusers.push({
+            //    username: user,
+            //    password: pass
+            //});
+            //$("#loginModal").modal('hide');
+            $location.path('/dashboard');
+          //  console.log($location.path());
+        } else {
+            $scope.message = "Invalid Login";
+        }
+    }
+});
---
+++ js/navbar.js
@@ -0,0 +1,9 @@
+﻿// toggle class scroll 
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+        $('.navbar-trans').addClass('afterscroll');
+    } else {
+        $('.navbar-trans').removeClass('afterscroll');
+    }
+
+});
